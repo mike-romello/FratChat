56c040b250e1fcafe624bbba9b99c6c4733a0052
Sal Casalena <salcasalena@gmail.com>
2024-11-02 12:06:54 -0400

    Implented frontend wrapper service for http calls

---
+++ frontend/src/app/services/http/http.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map } from 'rxjs';
+import { error } from 'node:console';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private baseUrl: string = "..."; // PLACE HOLDER
+
+  constructor(private http: HttpClient) { }
+
+  public get(url: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${url}`).pipe(
+      map((response) => response),
+      catchError((error) => {
+        throw error;
+      })
+    )
+  }
+
+  public post(url: string, data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/${url}`, data).pipe(
+      map((response) => response),
+      catchError((error) => {
+        throw error;
+      })
+    )
+  }
+
+  public put(url: string, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${url}`, data).pipe(
+      map((response) => response),
+      catchError((error) => {
+        throw error;
+      })
+    )
+  }
+
+  public delete(url: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${url}`).pipe(
+      map((response) => response),
+      catchError((error) => {
+        throw error;
+      })
+    )
+  }
+}
