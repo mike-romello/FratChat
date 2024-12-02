33b14e30d622ba35e57012d877cb441038f3449c
mike <https://gitlab.com/mikeromellodrexel/mr3658-michael-romello-se-181-001-course-project.git>
2024-12-01 19:42:13 -0500

    Saving progress b4 major overhaul

--- backend/setupDB.js
+++ backend/setupDB.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBN0qdEQAKWmMnh7G0df5njSjaKSTyYgfI",
+  authDomain: "fratchat-9f156.firebaseapp.com",
+  projectId: "fratchat-9f156",
+  storageBucket: "fratchat-9f156.firebasestorage.app",
+  messagingSenderId: "261943926955",
+  appId: "1:261943926955:web:0cc999cfd9a2b7c56b57ee",
+  measurementId: "G-4YQ4X5N2XB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // setupDB.js script for Node.js using pg module (PostgreSQL)
 const fs = require('fs');
 const { Client } = require('pg');
